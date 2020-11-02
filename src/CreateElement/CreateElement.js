fcbfa46a6af59b31a90db76aebb6fee4ef8ada49
yarishdmytro <yarish97@ukr.net>
2020-11-02 20:11:09 +0200

    Task1

---
+++ src/CreateElement/CreateElement.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CreateElement () {
+  return React.createElement('div', null, 'Hello, Create Element');
+}
+
+export default CreateElement;
---
+++ src/FunctionalComponent/FunctionalComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FuctionalComponent = () => {
+  return <div>Hello, FuctionalComponent</div>
+}
+
+export default FuctionalComponent;
